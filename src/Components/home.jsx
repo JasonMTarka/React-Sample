42a43e9d64100f7ba4cabc87350173d1831420c6
Jason Tarka <jasonmtarka@gmail.com>
2021-07-13 12:06:29 +0900

    Update home.jsx
    - Fixed old attribute names

--- src/Components/home.jsx
+++ src/Components/home.jsx
@@ -8,12 +8,12 @@ export default function Home({language}) {
   return (
     <div className="container">
       <h3 key="about">
-        {language === TEXT.JP_LANG ? HOME.JP.ABOUT : HOME.eng.ABOUT}
+        {language === TEXT.JP_LANG ? HOME.JP.ABOUT : HOME.ENG.ABOUT}
       </h3>
-      <p key="1">{language === TEXT.JP_LANG ? HOME.JP.P1 : HOME.eng.P1}</p>
-      <p key="2">{language === TEXT.JP_LANG ? HOME.JP.P2 : HOME.eng.P2}</p>
-      <p key="3">{language === TEXT.JP_LANG ? HOME.JP.P3 : HOME.eng.P3}</p>
-      <p key="4">{language === TEXT.JP_LANG ? HOME.JP.P4 : HOME.eng.P4}</p>
+      <p key="1">{language === TEXT.JP_LANG ? HOME.JP.P1 : HOME.ENG.P1}</p>
+      <p key="2">{language === TEXT.JP_LANG ? HOME.JP.P2 : HOME.ENG.P2}</p>
+      <p key="3">{language === TEXT.JP_LANG ? HOME.JP.P3 : HOME.ENG.P3}</p>
+      <p key="4">{language === TEXT.JP_LANG ? HOME.JP.P4 : HOME.ENG.P4}</p>
     </div>
   );
 }
