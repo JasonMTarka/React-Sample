de8363c36f54858ac21cd3ef216b4ddff301ca5b
Jason Tarka <jasonmtarka@gmail.com>
2021-07-14 11:39:01 +0900

    Update homeText.js
    - Added React-Router explanation

--- src/Text/homeText.js
+++ src/Text/homeText.js
@@ -3,14 +3,14 @@ export const HOME_TXT = {
     ABOUT: "About this Site",
     P1: "This is a sample React website created by Jason Tarka at HUI.",
     P2: "This is a Single Page Application (SPA) using React components to dynamically display data without repeated HTTP requests.",
-    P3: "By clicking the buttons on the navigation bar above, you can load new components into this space. As an example, I have included some small applications I created for learning purposes",
+    P3: "By clicking the buttons on the navigation bar above, you can load new page components into this space using React-Router. As an example, I have included some small applications I created for learning purposes",
     P4: "The formatting for this website is done through Bootstrap, an open-source CSS framework.",
   },
   JP: {
     ABOUT: "本サイトについて",
     P1: "本サイトは株式会社エイチューアイのジェイソン・ターカが作成したものです。",
     P2: "本サイトは Single Page Application（SPA)という仕組みで作られて、Reactの特徴であるコンポーネント（Component)を利用することで、普通のサイトほど複数のHTTPリクエストを送信する必要がないサイトです。",
-    P3: "上のナビゲーションバーのボタンを押すことで、ここで新しいコンポーネントを見ることができます。例として、私が作成した小さなアプリを本サイトに実装しておきました。よろしければご覧ください。",
+    P3: "上のナビゲーションバーのボタンを押すことで、React Routerを通して新しいページのコンポーネントを見ることができます。例として、私が作成した小さなアプリを本サイトに実装しておきました。よろしければご覧ください。",
     P4: "本サイトの見た目はBootstrapというオープンソースのCSSフレームワークで調整されています。",
   },
 };
